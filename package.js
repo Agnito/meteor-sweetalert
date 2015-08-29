41c0884583282a431ce61100dc278c3d187f694e
Dan Rubins <dan@agnitolabs.com>
2015-08-29 14:57:47 -0700

    Match package version to Sweet Alert version

--- package.js
+++ package.js
@@ -1,20 +1,16 @@
 Package.describe({
     name: 'kevohagan:sweetalert',
     summary: "a beautiful replacement for javascript's alert() ",
-    version: '1.0.0',
+    version: '1.1.0',
     git: 'https://github.com/kevohagan/meteor-sweetalert.git',
 });
 
 Package.onUse(function(api) {
 
-    api.versionsFrom("METEOR@0.9.1.1");
+    api.versionsFrom("METEOR@1.1.0.3");
 
     api.add_files([
         'sweetalert/lib/sweet-alert.css',
         'sweetalert/lib/sweet-alert.min.js'
     ], ['client']);
-
-    if(api.export){
-
-    }
-})
+});
